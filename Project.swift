9a492b4cb6c877809db300e40b084c8312c63660
Seunghun Yang <yabby1997@gmail.com>
2023-12-09 23:31:54 +0900

    Update Obscura/HaebitUI

--- Project.swift
+++ Project.swift
@@ -16,12 +16,12 @@ let targets: [Target] = [
         sources: ["Haebit/Sources/**"],
         resources: ["Haebit/Resources/**"],
         dependencies: [
+//            .project(target: "HaebitUI", path: "../../Feature/HaebitUI"),
 //            .project(target: "Obscura", path: "../../Feature/Obscura"),
 //            .project(target: "LightMeter", path: "../../Feature/LightMeter"),
-//            .project(target: "HaebitUI", path: "../../Feature/HaebitUI")
+            .package(product: "HaebitUI", type: .runtime),
             .package(product: "Obscura", type: .runtime),
             .package(product: "LightMeter", type: .runtime),
-            .package(product: "HaebitUI", type: .runtime)
         ],
         settings: .settings(
             base: ["DEVELOPMENT_TEAM": "5HZQ3M82FA"],
@@ -35,9 +35,9 @@ let project = Project(
     name: "Haebit",
     organizationName: "seunghun",
     packages: [
-        .remote(url: "https://github.com/Yabby1997/Obscura.git", requirement: .exact("0.1.0")),
+        .remote(url: "https://github.com/Yabby1997/HaebitUI.git", requirement: .exact("0.2.0")),
+        .remote(url: "https://github.com/Yabby1997/Obscura.git", requirement: .exact("0.2.0")),
         .remote(url: "https://github.com/Yabby1997/LightMeter.git", requirement: .exact("0.1.0")),
-        .remote(url: "https://github.com/Yabby1997/HaebitUI.git", requirement: .exact("0.1.0")),
     ],
     targets: targets
 )
