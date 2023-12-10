2902e270d7d6951cacbd1c139d55eb2f6f3e408f
Seunghun Yang <yabby1997@gmail.com>
2023-12-10 22:02:10 +0900

    Add Non-Exempt Encryption

--- Project.swift
+++ Project.swift
@@ -14,6 +14,7 @@ let targets: [Target] = [
                     "UIColorName": "LaunchScreenBackgroundColor",
                 ],
                 "UISupportedInterfaceOrientations": ["UIInterfaceOrientationPortrait"],
+                "ITSAppUsesNonExemptEncryption": false,
             ]
         ),
         sources: ["Haebit/Sources/**"],
