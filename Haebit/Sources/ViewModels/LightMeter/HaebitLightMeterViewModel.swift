1972f5c71303fb8050f8ecec81cc91a7ead87ff5
Seunghun Yang <yabby1997@gmail.com>
2024-01-07 21:51:38 +0900

    Fix unable to use light meter when HDR is not supported

--- Haebit/Sources/ViewModels/LightMeter/HaebitLightMeterViewModel.swift
+++ Haebit/Sources/ViewModels/LightMeter/HaebitLightMeterViewModel.swift
@@ -200,7 +200,7 @@ final class HaebitLightMeterViewModel: HaebitLightMeterViewModelProtocol {
         guard !camera.isRunning else { return }
         do {
             try await camera.setup()
-            try camera.setHDRMode(isEnabled: false)
+            try? camera.setHDRMode(isEnabled: false)
             bind()
         } catch {
             Task { @MainActor in
