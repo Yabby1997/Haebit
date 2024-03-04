a796d6301c79cdb5e0782fe48dcf1360ac7dcf59
Seunghun Yang <yabby1997@gmail.com>
2024-03-04 21:15:39 +0900

    Update MockRepository for HaebitLogger

--- Haebit/Sources/Implementations/MockHaebitLogRepository.swift
+++ Haebit/Sources/Implementations/MockHaebitLogRepository.swift
@@ -10,7 +10,6 @@ import Foundation
 import HaebitLogger
 
 class MockHaebitLogRepository: HaebitLogRepository {
-    let image = HaebitImage(photo: URL(string: "https://images.unsplash.com/photo-1565103446317-476a2b789651?q=80&w=2897&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")!, video: nil)
     let sanFrancisco = HaebitCoordinate(latitude: 37.7749, longitude: -122.4194)
     let sanFrancisco1 = HaebitCoordinate(latitude: 37.775051043038545, longitude: -122.41237811867396)
     let losAngeles = HaebitCoordinate(latitude: 34.0522, longitude: -118.2437)
@@ -24,7 +23,8 @@ class MockHaebitLogRepository: HaebitLogRepository {
                 id: UUID(),
                 date: Date(timeIntervalSince1970: 1706972400),
                 coordinate: sanFrancisco,
-                image: image,
+                image: "",
+                focalLength: 50,
                 iso: 100,
                 shutterSpeed: 30,
                 aperture: 2,
@@ -34,7 +34,8 @@ class MockHaebitLogRepository: HaebitLogRepository {
                 id: UUID(),
                 date: Date(timeIntervalSince1970: 1706764329),
                 coordinate: sanFrancisco1,
-                image: image,
+                image: "",
+                focalLength: 50,
                 iso: 100,
                 shutterSpeed: 60,
                 aperture: 1.4,
@@ -44,7 +45,8 @@ class MockHaebitLogRepository: HaebitLogRepository {
                 id: UUID(),
                 date: Date(timeIntervalSince1970: 1707220190),
                 coordinate: losAngeles,
-                image: image,
+                image: "",
+                focalLength: 50,
                 iso: 400,
                 shutterSpeed: 15,
                 aperture: 11,
@@ -54,7 +56,8 @@ class MockHaebitLogRepository: HaebitLogRepository {
                 id: UUID(),
                 date: Date(timeIntervalSince1970: 1707210190),
                 coordinate: losAngeles1,
-                image: image,
+                image: "",
+                focalLength: 50,
                 iso: 800,
                 shutterSpeed: 30,
                 aperture: 16,
@@ -64,7 +67,8 @@ class MockHaebitLogRepository: HaebitLogRepository {
                 id: UUID(),
                 date: Date(timeIntervalSince1970: 1707112990),
                 coordinate: losAngeles2,
-                image: image,
+                image: "",
+                focalLength: 50,
                 iso: 800,
                 shutterSpeed: 60,
                 aperture: 2,
@@ -74,7 +78,8 @@ class MockHaebitLogRepository: HaebitLogRepository {
                 id: UUID(),
                 date: Date(timeIntervalSince1970: 1707210210),
                 coordinate: losAngeles3,
-                image: image,
+                image: "",
+                focalLength: 50,
                 iso: 800,
                 shutterSpeed: 8,
                 aperture: 22,
@@ -95,5 +100,9 @@ class MockHaebitLogRepository: HaebitLogRepository {
     
     func save(log: HaebitLog) async throws {
         data.append(log)
+    }
+    
+    func remove(log: HaebitLog) async throws {
+        data.removeAll { $0 == log }
     }
 }
