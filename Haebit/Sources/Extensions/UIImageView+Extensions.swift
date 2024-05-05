fcf087f0b3924b55ac4a07e8f71fa50d705f9999
Seunghun Yang <yabby1997@gmail.com>
2024-05-05 20:22:15 +0900

    Modify downSampledImage to use task

--- Haebit/Sources/Extensions/UIImageView+Extensions.swift
+++ Haebit/Sources/Extensions/UIImageView+Extensions.swift
@@ -10,39 +10,38 @@ import UIKit
 
 extension UIImageView {
     func setDownSampledImage(at url: URL) {
-        DispatchQueue.main.async { [weak self] in
-            guard let self else { return }
-            let size = frame.size
-            let scale = window?.windowScene?.screen.scale ?? 3.0
-            
-            DispatchQueue.global().async {
-                guard let imageSource = CGImageSourceCreateWithURL(
-                    url as CFURL,
-                    [kCGImageSourceShouldCache: false] as CFDictionary
-                ) else {
-                    return
-                }
-                
-                let maxDimensionInPixels = max(size.width, size.height) * scale
-                let downsampleOptions = [
-                    kCGImageSourceCreateThumbnailFromImageAlways: true,
-                    kCGImageSourceShouldCacheImmediately: true,
-                    kCGImageSourceCreateThumbnailWithTransform: true,
-                    kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
-                ] as CFDictionary
-                
-                guard let cgImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions) else {
-                    return
-                }
-                
-                let downSampledImage = UIImage(cgImage: cgImage)
-                
-                DispatchQueue.main.async {
-                    UIView.animate(withDuration: 0.2) { [weak self] in
-                        self?.image = downSampledImage
-                    }
-                }
+        let size = frame.size
+        let scale = window?.windowScene?.screen.scale ?? 3.0
+        
+        Task.detached { [weak self] in
+            guard let imageSource = CGImageSourceCreateWithURL(
+                url as CFURL,
+                [kCGImageSourceShouldCache: false] as CFDictionary
+            ) else {
+                await self?.animateSet(image: nil, with: 0.2)
+                return
             }
+            
+            let maxDimensionInPixels = max(size.width, size.height) * scale
+            let downsampleOptions = [
+                kCGImageSourceCreateThumbnailFromImageAlways: true,
+                kCGImageSourceShouldCacheImmediately: true,
+                kCGImageSourceCreateThumbnailWithTransform: true,
+                kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
+            ] as CFDictionary
+
+            guard let cgImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions)else {
+                await self?.animateSet(image: nil, with: 0.2)
+                return
+            }
+            
+            await self?.animateSet(image: UIImage(cgImage: cgImage), with: 0.2)
+        }
+    }
+    
+    private func animateSet(image: UIImage?, with duration: TimeInterval) {
+        UIView.animate(withDuration: duration) { [weak self] in
+            self?.image = image
         }
     }
 }
