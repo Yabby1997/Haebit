c1f112b92eea5ec896462c0b406a43a177d87c41
Seunghun Yang <yabby1997@gmail.com>
2024-05-14 21:04:45 +0900

    Update make_project.sh for better readability

--- make_project.sh
+++ make_project.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-execute_tuist_command() {
-    echo "Executing command: $1"
-    $1
+execute_command() {
+    echo "----------------------------------------"
+    echo "🚀 $1"
+    $2
     if [ $? -eq 0 ]; then
         echo "✨ Success"
     else
-        echo "🚨 Failure"
+        echo "🚨 Failure"
         exit 1
     fi
+}
+
+fetch_dependencies() {
+    execute_command "Fetching external dependencies..." "tuist fetch"
+}
+
+cache_all() {
+    execute_command "Caching xcframeworks..." "tuist cache warm -x"
+}
+
+cache_except() {
+    execute_command "Caching xcframeworks..." "tuist cache warm -x --dependencies-only $1"
+}
+
+generate_target() {
+    execute_command "Generating project $1..." "tuist generate -x $1"
+}
+
+generate_target_without_cache() {
+    execute_command "Generating project $1 without cache..." "tuist generate $1"
 }
 
 cache_directory="$HOME/.tuist/Cache/BuildCache"
@@ -17,64 +38,53 @@ original_cache_folder=""
 
 disable_cache() {
     original_cache_folder=$(find "$cache_directory" -type d -name "$1" -exec dirname {} \;)
-    if [ -n "$original_cache_folder" ]; then
-        mv "$original_cache_folder" "$disabled_cache_folder"
-        echo "Cache $1 disabled."
-    else
-        echo "Cache not found."
-    fi
+    execute_command "Disabling $1..." "mv $original_cache_folder $disabled_cache_folder"
 }
 
 restore_cache() {
-    if [ -n "$disabled_cache_folder" ]; then
-        mv "$disabled_cache_folder" "$original_cache_folder"
-        echo "Cache $1 restored successfully."
-    else
-        echo "Disabled cache not found."
-    fi
+    execute_command "Restoring $1..." "mv $disabled_cache_folder $original_cache_folder"
 }
 
-# Function to execute commands for each option
 execute_option() {
     case $1 in
         1)
-            execute_tuist_command "tuist fetch"
-            execute_tuist_command "tuist cache warm -x"
-            execute_tuist_command "tuist generate -x HaebitDev"
+            fetch_dependencies
+            cache_all
+            generate_target "HaebitDev"
             ;;
         2)
-            execute_tuist_command "tuist fetch"
-            execute_tuist_command "tuist cache warm -x"
-            execute_tuist_command "tuist generate -x Haebit"
+            fetch_dependencies
+            cache_all
+            generate_target "Haebit"
             ;;
         3)
             cd LightMeterFeature || exit
-            execute_tuist_command "tuist fetch"
+            fetch_dependencies
             disable_cache "LightMeterFeature.xcframework"
-            execute_tuist_command "tuist cache warm -x LightMeterFeature --dependencies-only"
-            execute_tuist_command "tuist generate -x LightMeterFeatureDemo"
-            restore_cache "LightMeterFeature.xcframework"
+            cache_except "LightMeterFeature"
+            generate_target "LightMeterFeatureDemo"
+            restore_cache "LightMeterFeature"
             cd - || exit
             ;;
         4)
             cd LightMeterFeature || exit
-            execute_tuist_command "tuist fetch"
-            execute_tuist_command "tuist generate LightMeterFeatureDemo"
+            fetch_dependencies
+            generate_target_without_cache "LightMeterFeatureDemo"
             cd - || exit
             ;;
         5)
             cd FilmLogFeature || exit
-            execute_tuist_command "tuist fetch"
+            fetch_dependencies
             disable_cache "FilmLogFeature.xcframework"
-            execute_tuist_command "tuist cache warm -x FilmLogFeature --dependencies-only"
-            execute_tuist_command "tuist generate -x FilmLogFeatureDemo"
-            restore_cache "FilmLogFeature.xcframework"
+            cache_except "FilmLogFeature"
+            generate_target "FilmLogFeatureDemo"
+            restore_cache "FilmLogFeature"
             cd - || exit
             ;;
         6)
             cd FilmLogFeature || exit
-            execute_tuist_command "tuist fetch"
-            execute_tuist_command "tuist generate FilmLogFeatureDemo"
+            fetch_dependencies
+            generate_target_without_cache "FilmLogFeatureDemo"
             cd - || exit
             ;;
         *)
@@ -84,15 +94,13 @@ execute_option() {
     esac
 }
 
-echo "Choose a target:"
+echo "🎯 Choose a target to make project:"
 echo "1. HaebitDev"
 echo "2. Haebit"
 echo "3. LightMeterFeature"
 echo "4. LightMeterFeature (without cache)"
 echo "5. FilmLogFeature"
 echo "6. FilmLogFeature (without cache)"
-
+echo ""
 read -p "Enter your target: " choice
-
 execute_option $choice
-
