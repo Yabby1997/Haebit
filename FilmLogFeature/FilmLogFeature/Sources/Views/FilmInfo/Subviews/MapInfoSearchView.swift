036e0efdf4e0f2b601315b4bcc6b8bf62df86a02
Seunghun Yang <yabby1997@gmail.com>
2024-06-11 22:32:57 +0900

    Animate search result changes

--- FilmLogFeature/FilmLogFeature/Sources/Views/FilmInfo/Subviews/MapInfoSearchView.swift
+++ FilmLogFeature/FilmLogFeature/Sources/Views/FilmInfo/Subviews/MapInfoSearchView.swift
@@ -15,49 +15,53 @@ struct MapInfoSearchView<ViewModel: MapInfoViewModelProtocol>: View {
     
     var body: some View {
         VStack(spacing: 12) {
-            if viewModel.searchResults.isEmpty {
-                VStack {
-                    Spacer()
-                    Text("검색 결과가 없습니다.")
-                        .font(.system(size: 18, weight: .bold))
-                    Spacer()
-                }
-            } else {
-                ScrollView {
-                    Color.clear
-                        .frame(height: 12)
-                        .listRowInsets(.init(top: .zero, leading: .zero, bottom: .zero, trailing: .zero))
-                        .listRowBackground(Color.clear)
-                        .listRowSeparator(.hidden)
-                    ForEach(viewModel.searchResults, id:\.self) { result in
-                        HStack {
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text(result.name)
-                                    .font(.system(size: 14, weight: .bold))
-                                Text(result.address)
-                                    .font(.system(size: 12))
-                                    .foregroundStyle(.secondary)
-                            }
-                            Spacer()
-                        }
-                        .padding(.vertical, 1)
-                        .contentShape(Rectangle())
-                        .onTapGesture {
-                            viewModel.searchResultSelection = result
-                            dismiss()
-                        }
+            ZStack {
+                if viewModel.searchResults.isEmpty {
+                    VStack {
+                        Spacer()
+                        Text("검색 결과가 없습니다.")
+                            .font(.system(size: 18, weight: .bold))
+                        Spacer()
                     }
-                    Color.clear
-                        .frame(height: 40)
-                        .listRowInsets(.init(top: .zero, leading: .zero, bottom: .zero, trailing: .zero))
-                        .listRowBackground(Color.clear)
-                        .listRowSeparator(.hidden)
-                }
-                .scrollIndicators(.hidden)
-                .mask(alignment: .topLeading)  {
-                    LinearGradient(colors: [.black, .black.opacity(.zero)], startPoint: .init(x: .zero, y: 0.7), endPoint: .init(x: .zero, y: 1.0))
+                } else {
+                    ScrollView {
+                        Color.clear
+                            .frame(height: 12)
+                            .listRowInsets(.init(top: .zero, leading: .zero, bottom: .zero, trailing: .zero))
+                            .listRowBackground(Color.clear)
+                            .listRowSeparator(.hidden)
+                        ForEach(viewModel.searchResults, id:\.self) { result in
+                            HStack {
+                                VStack(alignment: .leading, spacing: 4) {
+                                    Text(result.name)
+                                        .font(.system(size: 14, weight: .bold))
+                                    Text(result.address)
+                                        .font(.system(size: 12))
+                                        .foregroundStyle(.secondary)
+                                }
+                                Spacer()
+                            }
+                            .padding(.vertical, 1)
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                viewModel.searchResultSelection = result
+                                dismiss()
+                            }
+                        }
+                        Color.clear
+                            .frame(height: 40)
+                            .listRowInsets(.init(top: .zero, leading: .zero, bottom: .zero, trailing: .zero))
+                            .listRowBackground(Color.clear)
+                            .listRowSeparator(.hidden)
+                    }
+                    .scrollIndicators(.hidden)
+                    .mask(alignment: .topLeading)  {
+                        LinearGradient(colors: [.black, .black.opacity(.zero)], startPoint: .init(x: .zero, y: 0.7), endPoint: .init(x: .zero, y: 1.0))
+                    }
                 }
             }
+            .transition(.slide)
+            .animation(.easeInOut(duration: 0.3), value: viewModel.searchResults)
             TextField("Search", text: $viewModel.searchQuery)
                 .multilineTextAlignment(.center)
                 .font(.system(size: 24, weight: .bold, design: .serif))
