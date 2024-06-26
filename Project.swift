0743114a9ec479c9f354f61e9ce3540a7ed06d7e
Seunghun Yang <yabby1997@gmail.com>
2024-06-27 01:24:15 +0900

    Version up to 1.3.1

--- Project.swift
+++ Project.swift
@@ -2,7 +2,7 @@ import ProjectDescription
 
 let majorVersion: Int = 1
 let minorVersion: Int = 3
-let patchVersion: Int = 0
+let patchVersion: Int = 1
 let versionString: Plist.Value = "\(majorVersion).\(minorVersion).\(patchVersion)"
 
 let plist: InfoPlist = .extendingDefault(
