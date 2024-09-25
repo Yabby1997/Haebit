b915d814764da85e0dc3439b8b16b02d7dba93af
Seunghun Yang <yabby1997@gmail.com>
2024-09-26 01:22:16 +0900

    Refine config UI with removing scenekit view and udsz file

--- ConfigFeature/ConfigFeature/Sources/Views/RootView/HaebitConfigView.swift
+++ ConfigFeature/ConfigFeature/Sources/Views/RootView/HaebitConfigView.swift
@@ -14,66 +14,85 @@ public struct HaebitConfigView: View {
     public var body: some View {
         NavigationStack {
             List {
-                Section {
-                    HStack {
-                        ZStack {
-                            FilmMagazineView()
-                            VStack {
+                Section {} header: {
+                    ZStack {
+                        VStack(spacing: 8) {
+                            Spacer()
+                            HStack {
+                                Text("Having fun with illumeter?")
+                                    .font(.system(size: 40, weight: .bold, design: .serif))
                                 Spacer()
-                                Text("illumeter is developed without profits.\nHow about buy me a roll of films?")
-                                    .multilineTextAlignment(.center)
+                            }
+                            HStack {
+                                Text("If so, consider to buy me a roll of film 🎞️. It would be very pleasant for me")
+                                    .font(.system(size: 16, weight: .semibold, design: .serif))
+                                Spacer()
                             }
                         }
-                        .frame(height: 200)
                     }
-                    .listRowInsets(EdgeInsets())
-                    .listRowBackground(Color.clear)
                     .onTapGesture {
-                        print("@@@")
+                        print("!!")
                     }
                 }
                 Section {
                     NavigationLink {
-                        Text("Apertures")
+                        Text("Aperture")
                     } label: {
                         HStack {
-                            Text("Apertures")
+                            Text("Aperture")
+                                .font(.system(size: 16, weight: .semibold))
                             Spacer()
-                            Text("12 values")
+                            Text("12 items")
+                                .font(.system(size: 14, design: .monospaced))
                         }
                     }
                     NavigationLink {
-                        Text("Shutter Speeds")
+                        Text("Shutter Speed")
                     } label: {
                         HStack {
-                            Text("Shutter Speeds")
+                            Text("Shutter Speed")
+                                .font(.system(size: 16, weight: .semibold))
                             Spacer()
-                            Text("8 values")
+                            Text("8 items")
+                                .font(.system(size: 14, design: .monospaced))
                         }
                     }
                     NavigationLink {
-                        Text("ISO Values")
+                        Text("ISO")
                     } label: {
                         HStack {
-                            Text("ISO Values")
+                            Text("ISO")
+                                .font(.system(size: 16, weight: .semibold))
                             Spacer()
-                            Text("4 values")
+                            HStack {
+                                Image(systemName: "lock.fill")
+                                Text("400")
+                            }
+                            .font(.system(size: 14, design: .monospaced))
                         }
                     }
                     NavigationLink {
-                        Text("Focal Lengths")
+                        Text("Focal Length")
                     } label: {
                         HStack {
-                            Text("Focal Lengths")
+                            Text("Focal Length")
+                                .font(.system(size: 16, weight: .semibold))
                             Spacer()
-                            Text("3 values")
+                            HStack {
+                                Image(systemName: "lock.fill")
+                                Text("50mm")
+                            }
+                            .font(.system(size: 14, design: .monospaced))
                         }
                     }
                 } header: {
                     HStack {
                         Image(systemName: "f.cursive")
-                        Text("Values")
+                        Text("Control")
                     }
+                    .font(.system(size: 14, weight: .bold))
+                } footer: {
+                    Text("At least a type of exposure related value should have more than two items.")
                 }
                 Section {
                     NavigationLink {
@@ -81,8 +100,10 @@ public struct HaebitConfigView: View {
                     } label: {
                         HStack {
                             Text("Aperture Ring")
+                                .font(.system(size: 16, weight: .semibold))
                             Spacer()
                             Text("Rigid")
+                                .font(.system(size: 14, design: .monospaced))
                         }
                     }
                     NavigationLink {
@@ -90,8 +111,10 @@ public struct HaebitConfigView: View {
                     } label: {
                         HStack {
                             Text("Shutter Speed Dial")
+                                .font(.system(size: 16, weight: .semibold))
                             Spacer()
                             Text("Heavy")
+                                .font(.system(size: 14, design: .monospaced))
                         }
                     }
                     NavigationLink {
@@ -99,8 +122,10 @@ public struct HaebitConfigView: View {
                     } label: {
                         HStack {
                             Text("ISO Dial")
+                                .font(.system(size: 16, weight: .semibold))
                             Spacer()
                             Text("Heavy")
+                                .font(.system(size: 14, design: .monospaced))
                         }
                     }
                     NavigationLink {
@@ -108,63 +133,82 @@ public struct HaebitConfigView: View {
                     } label: {
                         HStack {
                             Text("Focal Length Ring")
+                                .font(.system(size: 16, weight: .semibold))
                             Spacer()
                             Text("Soft")
+                                .font(.system(size: 14, design: .monospaced))
                         }
                     }
                 } header: {
                     HStack {
                         Image(systemName: "hand.draw")
-                        Text("Feedbacks")
+                        Text("Feedback")
                     }
+                    .font(.system(size: 14, weight: .bold))
                 }
                 Section {
                     NavigationLink {
-                        Text("Perforation Shape")
+                        Text("Perforation")
                     } label: {
                         HStack {
-                            Text("Perforation Shape")
+                            Text("Perforation")
+                                .font(.system(size: 16, weight: .semibold))
                             Spacer()
                             Text("Bell and Howell")
+                                .font(.system(size: 14, design: .monospaced))
                         }
                     }
                 } header: {
                     HStack {
                         Image(systemName: "sparkles")
-                        Text("Design")
+                        Text("Appearance")
                     }
+                    .font(.system(size: 14, weight: .bold))
                 }
                 Section {
                     NavigationLink {
                         Text("OpenSource")
                     } label: {
                         Text("OpenSource")
+                            .font(.system(size: 16, weight: .semibold))
                     }
                     NavigationLink {
                         Text("AppStore")
                     } label: {
                         Text("Review on AppStore")
+                            .font(.system(size: 16, weight: .semibold))
+                    }
+                    NavigationLink {
+                        Text("Buy me a film")
+                    } label: {
+                        Text("Buy me a film")
+                            .font(.system(size: 16, weight: .semibold))
                     }
                     NavigationLink {
                         Text("Contact")
                     } label: {
                         Text("Contact")
+                            .font(.system(size: 16, weight: .semibold))
                     }
                     HStack {
                         Text("App Version")
+                            .font(.system(size: 16, weight: .semibold))
                         Spacer()
                         Text("1.4.0")
+                            .font(.system(size: 14, design: .monospaced))
                     }
                 } header: {
                     HStack {
                         Image(systemName: "text.bubble")
-                        Text("Others")
+                        Text("Other")
                     }
+                    .font(.system(size: 14, weight: .bold))
                 }
             }
             .navigationTitle("Config")
             .navigationBarTitleDisplayMode(.inline)
             .scrollIndicators(.hidden)
+            .headerProminence(.increased)
             .toolbar {
                 ToolbarItem {
                     Button {
