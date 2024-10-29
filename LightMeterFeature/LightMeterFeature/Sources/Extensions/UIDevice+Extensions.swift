79c88bbceb0e962b512966ec513ec9f8ae6a1b1f
Seunghun Yang <yabby1997@gmail.com>
2024-10-29 21:31:34 +0900

    Support iPhone 16 series

--- LightMeterFeature/LightMeterFeature/Sources/Extensions/UIDevice+Extensions.swift
+++ LightMeterFeature/LightMeterFeature/Sources/Extensions/UIDevice+Extensions.swift
@@ -26,9 +26,11 @@ public extension UIDevice {
         case "iPhone11,2", "iPhone11,4", "iPhone11,6", "iPhone11,8", "iPhone12,1",
             "iPhone12,3", "iPhone12,5", "iPhone13,1", "iPhone13,2", "iPhone13,3",
             "iPhone13,4", "iPhone14,4", "iPhone14,5", "iPhone14,2", "iPhone14,3",
-            "iPhone14,7", "iPhone14,8", "iPhone15,4", "iPhone15,5":
+            "iPhone14,7", "iPhone14,8", "iPhone15,4", "iPhone15,5", "iPhone17,3",
+            "iPhone17,4":
             return 26
-        case "iPhone15,2", "iPhone15,3", "iPhone16,1", "iPhone16,2":
+        case "iPhone15,2", "iPhone15,3", "iPhone16,1", "iPhone16,2", "iPhone17,1",
+            "iPhone17,2":
             return 24
         default:
             return 28
